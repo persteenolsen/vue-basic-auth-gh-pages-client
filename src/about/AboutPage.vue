dc39a94c1c0ac8b1fb375e66443e874a2914f0ee
Per Steen Olsen <persteen22@hotmail.com>
2022-12-23 17:39:49 +0100

    A Vue client hosted at GitHib Pages

---
+++ src/about/AboutPage.vue
@@ -0,0 +1,96 @@
+
+
+<template>
+  
+  <div>
+      
+   <h3>Description</h3>
+
+  
+   This Vue.js + Vuex SPA Client at GitHub Pages work together with a secure ASP.NET Core 2.2 Web API at Azure App Service doing Basic Authentication 
+   <br/> <br/>
+   
+   <b>Last Updated</b>
+   <ul>
+    <li>23-12-2022</li>
+   </ul>
+
+   <b>Main functionality</b>
+   <ul>
+     
+      <li>Login using a Basic Authentication</li>
+           
+   </ul>
+
+   <b>Module Bundler</b>
+    <ul>
+      
+     <li>Manuel configuration of the Webpack 4 serving as a module bundler</li>
+
+   </ul>
+
+
+   <b>Security</b>
+    
+    <ul>
+   
+      <li>The login system is using a Basic Authentication</li>
+      <li>The user will have access to the secure endpoints of 
+          the Web API passing login and using the AuthData / Token</li>
+      </ul>
+
+ 
+    <b>The Vue.js + Vuex Single Page Application - The frontend</b>
+   
+     <ul>
+    
+      <li>Vue.js</li>
+      <li>Vuex for state management</li>
+      <li>HTML and CSS</li>
+      <li>Mainly JavaScript ES5, ES6 and ES7</li>
+      <li>Traditionel Bootstrap 4 by CDN for the responsive design</li>
+  
+   </ul>
+
+   <b>The ASP.NET Core 2.2 Web API - The backend</b>
+    
+    <ul>
+  
+    <li>ASP.NET Core 2.2 Web API </li>
+    
+  
+   </ul>
+
+
+  <b>Hosting</b>
+  <ul>
+    <li>The Vue.js + Vuex SPA client is hosted at GitHub Pages</li>
+    <li>The Web API is hosted at Azure App Service</li>
+  </ul>
+
+  <b>Text Editor</b>
+  <ul>
+
+   <li>Visual Studio Code as Text Editor</li>
+
+ </ul>
+
+ <b>Testing</b>
+ <ul>
+   <li>Test Driven Developement and Unit Testing all the way developing the system</li>
+ </ul>
+
+   
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
